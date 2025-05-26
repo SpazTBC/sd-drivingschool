c570c6d92bafff1b7ee2772af1b2d69cc874f1b0
SpazTBC <FameRaider@gmail.com>
2025-05-26 00:28:15 -0500

    new update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Shawns Developments'
 description 'SD Driving School - Complete driving school system with multiple license types'
-version '1.0.0'
+version '2.0.0'
 
 shared_scripts {
     'config.lua',
